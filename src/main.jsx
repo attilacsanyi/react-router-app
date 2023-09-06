0246a9e20713f08aaf5bae95506d5eb419ef69a1
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2023-09-06 10:23:06 +0200

    chore: format

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 const router = createBrowserRouter([
